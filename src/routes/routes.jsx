f3f9067c59202a237897bc4377feba17c31dc1f9
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-03-19 15:27:53 +0600

    Features: Login & Sign Up Page Implenmented

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,32 +1,53 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import Tasks from '../pages/Tasks';
-import Chat from '../pages/Chat';
-import Settings from '../pages/Settings';
-import Profile from '../pages/Profile';
+import App from "../App";
+import Chat from "../pages/Chat";
+import Login from "../pages/Login";
+import Tasks from "../pages/Tasks";
+import Signup from "../pages/Signup";
+import Profile from "../pages/Profile";
+import Archive from "../pages/Archive";
+import Settings from "../pages/Settings";
+import { createBrowserRouter } from "react-router-dom";
+import PrivateRoute from "../components/layouts/PrivateRoute";
+
 const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Tasks />,
-      },
-      {
-        path: '/chat',
-        element: <Chat />,
-      },
-      {
-        path: '/settings',
-        element: <Settings />,
-      },
-      {
-        path: '/profile',
-        element: <Profile />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: (
+			<PrivateRoute>
+				<App />
+			</PrivateRoute>
+		),
+		children: [
+			{
+				index: true,
+				element: <Tasks />,
+			},
+			{
+				path: "/archive",
+				element: <Archive />,
+			},
+			{
+				path: "/chat",
+				element: <Chat />,
+			},
+			{
+				path: "/settings",
+				element: <Settings />,
+			},
+			{
+				path: "/profile",
+				element: <Profile />,
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <Signup />,
+	},
 ]);
 
 export default routes;
